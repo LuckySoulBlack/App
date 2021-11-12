e3e5b9984c52d0887266d0c1d10ddda242f24a78
Lucksoulblack <lucknaite@gmail.com.br>
2021-11-12 01:05:07 -0300

    splash

---
+++ app/src/main/java/com/example/snipercampo/AberturaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.snipercampo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AberturaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_abertura)
+    }
+}
